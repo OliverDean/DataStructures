650089fe2b5e2991bbbc99b7865d01d857aaa36e
Oliver <21307131@student.uwa.edu.au>
2023-04-29 14:11:35 +0800

    update

---
+++ project/CITS2200ProjectTester.java
@@ -0,0 +1,35 @@
+package project;
+
+import java.io.*;
+import java.util.*;
+
+public class CITS2200ProjectTester {
+	public static void loadGraph(CITS2200Project project, String path) {
+		// The graph is in the following format:
+		// Every pair of consecutive lines represent a directed edge.
+		// The edge goes from the URL in the first line to the URL in the second line.
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(path));
+			while (reader.ready()) {
+				String from = reader.readLine();
+				String to = reader.readLine();
+				System.out.println("Adding edge from " + from + " to " + to);
+				project.addEdge(from, to);
+			}
+		} catch (Exception e) {
+			System.out.println("There was a problem:");
+			System.out.println(e.toString());
+		}
+	}
+
+	public static void main(String[] args) {
+		// Change this to be the path to the graph file.
+		String pathToGraphFile = "/path/to/the/file";
+		// Create an instance of your implementation.
+		CITS2200Project proj = new MyCITS2200Project();
+		// Load the graph into the project.
+		loadGraph(proj, pathToGraphFile);
+
+		// Write your own tests!
+	}
+}
